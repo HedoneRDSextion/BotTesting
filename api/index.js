fdce8b1ab2c2aa9a628578a0bb5a474307c38797
Francisco Forte <francisco.forte@mecwide.com>
2025-06-02 18:27:39 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import { openai } from "./utils/openai.js";
+import { openai } from "../utils/openai.js";
 
 const app = express();
 app.use(express.json());
